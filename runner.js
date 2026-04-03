f93ad1c4a2bfe603777ebf6e9ddeeb8b5d5ddbcb
Dominic Cassidy <dominic@cassidy.casa>
2026-04-03 11:55:04 +0100

    Move runner data to corestore

--- runner.js
+++ runner.js
@@ -1,17 +1,25 @@
 const process = require('process')
-const fs = require('fs')
+const Corestore = require('corestore')
 
-const payloadFile = process.argv[2]
-const payload = JSON.parse(fs.readFileSync(payloadFile, 'utf-8'))
-fs.unlinkSync(payloadFile)
-const { source, data, bootstrap, id, hostId } = payload
+const runtimeId = process.argv[2]
 
-const log = console.log
-console.log = (...args) => log(hostId === 'h1' ? '[Bootstrap]' : `[Host ${hostId}]`, ...args)
+async function main() {
+  const store = new Corestore('.runtime', { readOnly: true })
+  const core = store.get({ name: runtimeId, valueEncoding: 'json' })
+  await core.ready()
 
-const fn = new Function('require', 'return ' + source)(require)
-const controller = require('mininet/host')
-fn({ data, bootstrap, id, controller }).catch((err) => {
-  console.error(err)
-  process.exitCode = 1
-})
+  const { data, bootstrap, id, hostId, source } = await core.get(0)
+  await store.close()
+
+  const log = console.log
+  console.log = (...args) => log(hostId === 'h1' ? '[Bootstrap]' : `[Host ${hostId}]`, ...args)
+  const fn = new Function('require', 'return ' + source)(require)
+  const controller = require('mininet/host')
+
+  fn({ data, bootstrap, id, controller }).catch(async (err) => {
+    console.error(err)
+    process.exitCode = 1
+  })
+}
+
+main()
